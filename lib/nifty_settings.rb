ecf8e826dcc90951626adf5196304894803bb557
Manuel Meurer <manuel@krautcomputing.com>
2014-06-21 17:51:18 +0200

    Move require

--- lib/nifty_settings.rb
+++ lib/nifty_settings.rb
@@ -1,8 +1,7 @@
-require 'nifty_settings/version'
-
 module NiftySettings
 end
 
+require 'nifty_settings/version'
 require 'nifty_settings/settings'
 require 'nifty_settings/railtie' if defined?(Rails)
 
